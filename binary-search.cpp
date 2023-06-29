795bebe16f9d784aa8f42c5aead40fe0b8cce88a
Raunak Nag <85424111+Raunak2024@users.noreply.github.com>
2023-06-29 10:36:26 +0530

    Add files via upload

---
+++ binary-search.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //binary search (components)
+    //->search space
+    //->target value
+    //binary search follows the montonicity condition, i.e. it can be applied on places where the first half of the array is different from the second half of the array.
+    //binary search is generally applied on arrays where we can traverse a list of items
+    //binary search kind of divides the search space into two halves depending upon the mid element until it finds the required value or it finishes traversing the whole search space. 
+    //binary search has a time compleixty of o(logn).
+    //if array is sorted-> apply two pointers/three pointers/sliding window/binary search.
+    int mid=low+((mid-low)/2);
+    int m;
+    
+}
